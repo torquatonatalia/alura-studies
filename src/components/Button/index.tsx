6c05b7cb9047803881d779fc398365a7bfdce082
torquatonatalia <natalia.torquato.duarte@gmail.com>
2023-01-22 20:51:40 -0300

    Estilizando o componente Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './style.scss';
 
 class Button extends React.Component {
     render() {
         return (
-            <button>
+            <button className='botao'>
                 Botao
             </button>
         )
