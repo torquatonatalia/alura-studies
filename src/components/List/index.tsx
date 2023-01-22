46acda3453ae31d406bafc19f3da5cfb5a6fcb84
torquatonatalia <natalia.torquato.duarte@gmail.com>
2023-01-22 20:56:58 -0300

    Estilizando o componente List

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './style.scss';
 
 function List() {
     const tarefas = [
@@ -12,13 +13,13 @@ function List() {
       }
     ]
     return (
-        <aside>
+        <aside className="listaTarefas">
             <h2>
                 Estudos do dia
             </h2>
             <ul>
                 {tarefas.map((item, index) => (
-                    <li key={index}>
+                    <li key={index} className="item">
                         <h3>
                             {item.tarefa}
                         </h3>
