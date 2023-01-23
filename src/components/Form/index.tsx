96e051c50e26b2cfc17d99fcfc4acdde587528fc
torquatonatalia <natalia.torquato.duarte@gmail.com>
2023-01-23 20:55:43 -0300

    Refotorando o estilo da página Form com CSS Module

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import Button from '../Button';
-import './style.scss';
+import style from './Form.module.scss';
 
 class Form extends React.Component {
     render() {
         return (
-            <form className='novaTarefa'>
-                <div className='inputContainer'>
+            <form className={style.novaTarefa}>
+                <div className={style.inputContainer}>
                     <label htmlFor="tarefa">
                         Adicione um novo estudo
                     </label>
                     <input type="text" name="tarefa" id="tarefa" placeholder="O que você quer estudar" required />
                 </div>
-                <div className='inputContainer'>
+                <div className={style.inputContainer}>
                     <label htmlFor="tempo">
                         Tempo
                     </label>
