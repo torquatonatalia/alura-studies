0e87188f341ae469bb33247736313389108feb39
torquatonatalia <natalia.torquato.duarte@gmail.com>
2023-01-24 19:51:06 -0300

    Testando o useState na List

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Item from './Item';
 import style from './List.module.scss';
 
 function List() {
-    const tarefas = [
-      {
-        tarefa: 'React',
-        tempo: '02:00:00'
-      },
-      {
-        tarefa: 'Javascript',
-        tempo: '01:00:00'
-      }
-    ]
+    const [tarefas, setTarefas] = useState(
+        [
+            {
+                tarefa: 'React',
+                tempo: '02:00:00'
+            },
+            {
+                tarefa: 'Javascript',
+                tempo: '01:00:00'
+            }
+        ]
+    );
     return (
         <aside className={style.listaTarefas}>
-            <h2>
+            <h2 onClick={() => {
+                setTarefas([...tarefas, { tarefa: "Estudar estados", tempo: "05:00:00" }]);
+            }}>
                 Estudos do dia
             </h2>
             <ul>
                 {tarefas.map((item, index) => (
-                    <Item 
-                    key={index}
+                    <Item
+                        key={index}
                         {...item}
                     />
                 ))}
